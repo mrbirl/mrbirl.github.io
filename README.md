3eeba22e30c91ac35e4a323be08614d2d5889256
Cian Brassil <mrb@live.ie>
2016-05-06 18:38:05 +0100

    Initial site commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mrbirl.github.io
 Personal website - brassil.net.
+
+
+Based on <a href="http://webdesign.tutsplus.com/tutorials/building-the-simple-instagram-based-portfolio--cms-22243">this</a> tutorial, by Steve Everett.
---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    //Set up instafeed
+    var feed = new Instafeed({
+        clientId: 'fa0113f5036e4072b3e508ac03876570',
+        target: 'instafeed',
+        get: 'user',
+        userId: 'mrbirl',
+        links: true,
+        //limit: 8,
+        sortBy: 'most-recent',
+        resolution: 'standard_resolution',
+        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+    });
+    feed.run();
+
+});
